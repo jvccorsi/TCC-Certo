1e0308ce2e39abe5b90408a987d4e2779b169cbd
João Victor Corsi Ferreira <32347104+jvccorsi@users.noreply.github.com>
2022-03-03 08:34:59 -0300

    Com classname

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { styled } from '@mui/material/styles';
+// import { styled } from '@mui/material/styles';
 // import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
+// import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
 import styles from './Home.module.css';
