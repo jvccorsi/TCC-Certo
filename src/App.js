12da92422157268ee7d23f39fbe8be4fc613a3a7
João Victor Corsi Ferreira <32347104+jvccorsi@users.noreply.github.com>
2022-03-03 00:46:20 -0300

    limpando o proj

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Typography from '@mui/material/Typography';
 
 function App() {
   return (
     <div className="App">
-      <h1>TCC do corsi em construção</h1>
+      <Typography
+        variant="h1"
+        component="div"
+        style={{ fontFamily: 'Hubballi' }}
+        align="center"
+        color="#6AFA82"
+      >
+        Tcc do corsi
+      </Typography>
     </div>
   );
 }
