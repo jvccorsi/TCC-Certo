d724e1edd94f83905be1691878d9f2224bdd3ec2
João Victor Corsi Ferreira <32347104+jvccorsi@users.noreply.github.com>
2022-03-03 08:11:24 -0300

    aa

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
+// import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
-import styles from './Home.module.css';
+// import styles from './Home.module.css';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: 'red',
