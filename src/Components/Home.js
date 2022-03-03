db9dfab50285b7a6d2f34337916ba55bbea128dd
João Victor Corsi Ferreira <32347104+jvccorsi@users.noreply.github.com>
2022-03-03 08:34:42 -0300

    Com classname

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,26 +4,17 @@ import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
-// import styles from './Home.module.css';
-
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: 'red',
-  padding: '20px',
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-  margin: '0',
-  height: '100vh',
-}));
+import styles from './Home.module.css';
 
 const Home = () => {
   return (
     <Container>
-      <Grid container spacing={2}>
+      <Grid container spacing={9}>
         <Grid item xs={8} md={7}>
-          <Item>xs=6 md=4</Item>
+          <p className={styles.grid}>xs=6 md=4</p>
         </Grid>
         <Grid item xs={4} md={5}>
-          <Item>xs=6 md=4</Item>
+          <p className={styles.grid2}>xs=6 md=4</p>
         </Grid>
       </Grid>
     </Container>
