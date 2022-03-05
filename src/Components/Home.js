c46845b6a7ce3e40d0c63e75291b452b51c7034f
João Victor Corsi Ferreira <32347104+jvccorsi@users.noreply.github.com>
2022-03-05 01:20:42 -0300

    update tela login

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,34 +4,37 @@ import Grid from '@mui/material/Grid';
 import styles from './Home.module.css';
 import GoogleIcon from '@mui/icons-material/Google';
 import { Link } from 'react-router-dom';
-import { TextField, Button } from '@mui/material';
+import { TextField, Button, Container } from '@mui/material';
 import Checkbox from '@mui/material/Checkbox';
 
 const Home = () => {
   return (
-    <div>
-      <Grid container className={styles.grid_content}>
-        <Grid item xs={7} sm={7}>
-          <div className={styles.tratar_img}>
-            <img
-              src={minhaImagem}
-              alt="research_undraw"
-              className={styles.img}
-            />
-          </div>
-        </Grid>
+    <main>
+      <Container maxWidth="xl">
+        <Grid container className={styles.grid_content}>
+          <Grid item xs={7} sm={7}>
+            <div className={styles.tratar_img}>
+              <img
+                src={minhaImagem}
+                alt="research_undraw"
+                className={styles.img}
+              />
+            </div>
+          </Grid>
 
-        <Grid item container xs={5} sm={5}>
-          <div className={styles.flex_direction_centralize}>
-            <div className={styles.infos_login}>
-              <div className={styles.title_home}>
-                <h1 className={styles.text}>Welcome to DataTox</h1>
-                <h3 className={styles.text}>
-                  New to DataTox?{' '}
-                  <span style={{ color: 'blue' }}> Create an account</span>
-                </h3>
-
-                <div>
+          <Grid item xs={5} sm={5}>
+            <Container
+              component="article"
+              maxWidth="sm"
+              className={styles.flex_direction_centralize}
+            >
+              <div className={styles.infos_login}>
+                <div className={styles.title_home}>
+                  <h1 className={styles.text}>Welcome to DataTox</h1>
+                  <h3 className={styles.text}>
+                    New to DataTox?{' '}
+                    <span style={{ color: 'blue' }}> Create an account</span>
+                  </h3>
                   <Button
                     style={{
                       color: 'black',
@@ -46,11 +49,9 @@ const Home = () => {
                     Google
                   </Button>
                 </div>
-              </div>
 
-              <div className={styles.form_login}>
-                <form>
-                  <div>
+                <div className={styles.form_login}>
+                  <form>
                     <TextField
                       id="Username"
                       label="Username"
@@ -62,8 +63,7 @@ const Home = () => {
                       required
                       className={styles.textfield_options}
                     />
-                  </div>
-                  <div>
+
                     <TextField
                       label="Senha"
                       margin="normal"
@@ -72,44 +72,44 @@ const Home = () => {
                       required
                       className={styles.textfield_options}
                     />
-                  </div>
 
-                  <Grid container className={styles.button}>
-                    <Grid item xs={7} sm={6}>
-                      <div
-                        style={{
-                          display: 'flex',
-                          alignItems: 'center',
-                          height: 20,
-                        }}
+                    <Grid container className={styles.button}>
+                      <Grid item xs={7} sm={6}>
+                        <div
+                          style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            height: 20,
+                          }}
+                        >
+                          <Checkbox defaultChecked />
+                          <p>Remember this Device</p>
+                        </div>
+                      </Grid>
+                      <Grid item xs={5} sm={6}>
+                        <Link to="/login/perdeu">Perdeu a senha? </Link>
+                      </Grid>
+                    </Grid>
+
+                    <div className={styles.textfield_options}>
+                      <Button
+                        type="submit"
+                        variant="contained"
+                        style={{ backgroundColor: '#FE6969' }}
+                        size="large"
+                        className={styles.textfield_options}
                       >
-                        <Checkbox defaultChecked />
-                        <p>Remember this Device</p>
-                      </div>
-                    </Grid>
-                    <Grid item xs={7} sm={6}>
-                      <Link to="/login/perdeu">Perdeu a senha? </Link>
-                    </Grid>
-                  </Grid>
-
-                  <div className={styles.textfield_options}>
-                    <Button
-                      type="submit"
-                      variant="contained"
-                      style={{ backgroundColor: '#FE6969' }}
-                      size="large"
-                      className={styles.textfield_options}
-                    >
-                      Próximo
-                    </Button>
-                  </div>
-                </form>
+                        Login
+                      </Button>
+                    </div>
+                  </form>
+                </div>
               </div>
-            </div>
-          </div>
+            </Container>
+          </Grid>
         </Grid>
-      </Grid>
-    </div>
+      </Container>
+    </main>
   );
 };
 
