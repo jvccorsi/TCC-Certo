95d6f4ec31544cbabec80582dcfa443fa183cf04
João Victor Corsi Ferreira <32347104+jvccorsi@users.noreply.github.com>
2022-03-05 17:37:03 -0300

    Error finished

--- src/Components/Error404.js
+++ src/Components/Error404.js
@@ -1,7 +1,36 @@
+import { Typography, Container, Box, Button } from '@mui/material';
 import React from 'react';
+import minhaImagem from '../Assets/undraw_page_not_found_re_e9o6 1.svg';
+import styles from './Error.module.css';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Error404 = () => {
-  return <div>Error404</div>;
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate('/home');
+  }
+  return (
+    <Container maxWidth="xl" align="center" className={styles.container_error}>
+      <Typography variant="h4" align="center" m={2}>
+        Sorry, page not found !
+      </Typography>
+      <div className={styles.error_img}>
+        <img src={minhaImagem} alt="research_undraw" />
+      </div>
+      <Box mt={3}>
+        <Button
+          type="submit"
+          variant="contained"
+          size="large"
+          className={styles.button_error}
+          onClick={handleClick}
+        >
+          Voltar
+        </Button>
+      </Box>
+    </Container>
+  );
 };
 
 export default Error404;
