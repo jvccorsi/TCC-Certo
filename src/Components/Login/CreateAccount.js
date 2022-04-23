df19b9edf344d2e1f0f7a86cc243795945b0846b
João Victor Corsi Ferreira <32347104+jvccorsi@users.noreply.github.com>
2022-04-22 22:12:43 -0300

    deploy

--- src/Components/Login/CreateAccount.js
+++ src/Components/Login/CreateAccount.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useState } from 'react';
 import styles from './LoginForm.module.css';
 import { Link } from 'react-router-dom';
 import {
